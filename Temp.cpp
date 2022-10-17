7397e44dbdcc9febec0074c4a5a82fc2dc3a0cdc
Akshay Nagar <akshay.nagar0101@outlook.com>
2022-10-17 12:46:51 +0530

    Updated 17th October

--- Temp.cpp
+++ Temp.cpp
@@ -6,41 +6,17 @@
 #define pq priority_queue
 #define pb push_back
 #define ld long double
-#define REP(x,n) for (int x = 0; x < n; x++)
+#define REP(x, n) for (int x = 0; x < n; x++)
 
 using namespace std;
 
 class Solution {
-    public:
-        
-
+   public:
 };
 
 int main() {
+// //#endif
 
-#ifndef ONLINE_JUDGE
-    freopen("Input1.txt","r", stdin);
-    freopen("Output1.txt","w", stdout);
-#endif
-
-    int T;
-    cin >> T;
-    while (T--) {
-        Solution ob;
-        int n;
-        cin >> n;
-        vector<lli> v;
-        map<lli, lli> mp;
-        REP(x,n){
-            lli t;
-            cin >> t;
-            mp[t]++;
-        }
-        lli ans = 0;
-        for(auto it:mp){
-            ans += ((it.second * (it.second - 1)) / 2);
-        }
-        cout << ans << endl;
-    }
+    cout << (4194304%mod);
     return 0;
 }
